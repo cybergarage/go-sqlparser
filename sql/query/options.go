0f83b76e2f1e143230751a35aea3cb46019796d5
Satoshi Konno <skonno@cybergarage.org>
2023-06-22 19:14:01 +0900

    Add query.IfNotExists()

---
+++ sql/query/options.go
@@ -0,0 +1,32 @@
+// Copyright (C) 2022 The go-sqlparser Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package query
+
+// IfNotExists represents a IF NOT EXISTS option.
+type IfNotExists struct {
+	enabled bool
+}
+
+// NewIndex returns a new Index instance.
+func NewIfNotExistsWith(v bool) *IfNotExists {
+	return &IfNotExists{
+		enabled: v,
+	}
+}
+
+// IfNotExists returns the IF NOT EXISTS option.
+func (opt *IfNotExists) IfNotExists() bool {
+	return opt.enabled
+}
