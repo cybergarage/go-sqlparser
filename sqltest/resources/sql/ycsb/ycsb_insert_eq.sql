8a9545fb4aba3ce21f46ad062e21b239c9f0fa96
Satoshi Konno <skonno@cybergarage.org>
2023-07-21 01:37:53 +0900

    Add ycsb_insert_eq.sql

---
+++ sqltest/resources/sql/ycsb/ycsb_insert_eq.sql
@@ -0,0 +1,12 @@
+INSERT INTO usertable VALUES (
+    YCSB_KEY = 'key001', 
+    FIELD0 = 'field000',
+    FIELD1 = 'field001',
+    FIELD2 = 'field002',
+    FIELD3 = 'field003',
+    FIELD4 = 'field004',
+    FIELD5 = 'field005',
+    FIELD6 = 'field006',
+    FIELD7 = 'field007',
+    FIELD8 = 'field008',
+    FIELD9 = 'field009')
