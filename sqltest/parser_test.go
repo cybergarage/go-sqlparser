8a8babd7f1afa9af7820c72693e82e74b198e227
Satoshi Konno <skonno@cybergarage.org>
2023-07-30 11:52:10 +0900

    Update TestExtraQueries()

--- sqltest/parser_test.go
+++ sqltest/parser_test.go
@@ -56,6 +56,7 @@ func TestDropQueries(t *testing.T) {
 func TestExtraQueries(t *testing.T) {
 	res := []string{
 		".*copy.*\\.sql",
+		".*commit.*\\.sql",
 	}
 	for _, re := range res {
 		testQueryDirectoryWithRegex(t, sqlTestResourceQueriesDirectory, re)
