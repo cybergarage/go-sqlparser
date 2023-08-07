f8eda8c148a3b3918b3bd991df1acd6ea19ca11e
Satoshi Konno <skonno@cybergarage.org>
2023-08-07 09:02:13 +0900

    Add pgbench_vacuum.sql

---
+++ sqltest/resources/pgbench/pgbench_vacuum.sql
@@ -0,0 +1,6 @@
+vacuum analyze pgbench_branches;
+vacuum analyze pgbench_tellers;
+vacuum analyze pgbench_accounts;
+vacuum analyze pgbench_history;
+vacuum pgbench_branches;
+vacuum pgbench_tellers;
