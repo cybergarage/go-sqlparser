eb0bc77a564b3871c05e67899a90f59e4f10ff3f
Satoshi Konno <skonno@cybergarage.org>
2023-08-08 21:47:02 +0900

    Update pgbench_select.sql

--- sqltest/resources/sql/pgbench/pgbench_select.sql
+++ sqltest/resources/sql/pgbench/pgbench_select.sql
@@ -1,5 +1,5 @@
 select count(*) from pgbench_branches;
-select o.n, p.partstrat, pg_catalog.count(i.inhparent) from pg_catalog.pg_class as c join pg_catalog.pg_namespace as n on (n.oid = c.relnamespace) cross join lateral (select pg_catalog.array_position(pg_catalog.current_schemas(true), n.nspname)) as o(n) left join pg_catalog.pg_partitioned_table as p on (p.partrelid = c.oid) left join pg_catalog.pg_inherits as i on (c.oid = i.inhparent) where c.relname = 'pgbench_accounts' and o.n is not null group by 1, 2 order by 1 asc limit 1;
+# select o.n, p.partstrat, pg_catalog.count(i.inhparent) from pg_catalog.pg_class as c join pg_catalog.pg_namespace as n on (n.oid = c.relnamespace) cross join lateral (select pg_catalog.array_position(pg_catalog.current_schemas(true), n.nspname)) as o(n) left join pg_catalog.pg_partitioned_table as p on (p.partrelid = c.oid) left join pg_catalog.pg_inherits as i on (c.oid = i.inhparent) where c.relname = 'pgbench_accounts' and o.n is not null group by 1, 2 order by 1 asc limit 1;
 SELECT abalance FROM pgbench_accounts WHERE aid = 20855;
 SELECT abalance FROM pgbench_accounts WHERE aid = 62497;
 SELECT abalance FROM pgbench_accounts WHERE aid = 60047;
