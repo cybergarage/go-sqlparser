18fad0e8809e4f8a3e9a9263028b970bacb03463
Satoshi Konno <skonno@cybergarage.org>
2023-08-23 09:08:13 +0900

    Add query.OrderBy

--- sql/query/order.go
+++ sql/query/order.go
@@ -37,3 +37,69 @@ func (t Order) String() string {
 		return "NONE"
 	}
 }
+
+// OrderBy represents an ORDER BY clause.
+type OrderBy struct {
+	name  string
+	order Order
+}
+
+// NewOrderBy returns a new OrderBy instance.
+func NewOrderBy() *OrderBy {
+	return &OrderBy{
+		name:  "",
+		order: OrderNone,
+	}
+}
+
+func NewOrderByWith(name string, order Order) *OrderBy {
+	return &OrderBy{
+		name:  name,
+		order: order,
+	}
+}
+
+// SetName sets the name.
+func (orderBy *OrderBy) SetName(name string) *OrderBy {
+	orderBy.name = name
+	return orderBy
+}
+
+// SetOrder sets the order.
+func (orderBy *OrderBy) SetOrder(order Order) *OrderBy {
+	orderBy.order = order
+	return orderBy
+}
+
+// Name returns the name.
+func (orderBy *OrderBy) Name() string {
+	return orderBy.name
+}
+
+// Order returns the order.
+func (orderBy *OrderBy) Order() Order {
+	return orderBy.order
+}
+
+// IsNone returns true whether the order is none.
+func (orderBy *OrderBy) IsNone() bool {
+	return orderBy.order == OrderNone
+}
+
+// IsAsc returns true whether the order is asc.
+func (orderBy *OrderBy) IsAsc() bool {
+	return orderBy.order == OrderAsc
+}
+
+// IsDesc returns true whether the order is desc.
+func (orderBy *OrderBy) IsDesc() bool {
+	return orderBy.order == OrderDesc
+}
+
+// String returns the string representation.
+func (orderBy *OrderBy) String() string {
+	if orderBy.IsNone() {
+		return ""
+	}
+	return "ORDER BY" + orderBy.name + " " + orderBy.order.String()
+}
