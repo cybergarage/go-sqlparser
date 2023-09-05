f2b4ed008dae5fae44f8caa590e796d45f539731
Satoshi Konno <skonno@cybergarage.org>
2023-09-05 12:49:12 +0900

    Update data_type.pict

--- sqltest/res/sql/sql92/create_table.sql
+++ sqltest/res/sql/sql92/create_table.sql
@@ -1,10 +1,28 @@
 CREATE TABLE test (
 	ctext TEXT PRIMARY KEY,
-	cint INT
+	cint INT,
+	cfloat FLOAT,
+	cdouble DOUBLE
 );
 
 CREATE TABLE test (
 	ctext TEXT,
-	cint INT PRIMARY KEY
+	cint INT PRIMARY KEY,
+	cfloat FLOAT,
+	cdouble DOUBLE
+);
+
+CREATE TABLE test (
+	ctext TEXT,
+	cint INT,
+	cfloat FLOAT PRIMARY KEY,
+	cdouble DOUBLE
+);
+
+CREATE TABLE test (
+	ctext TEXT,
+	cint INT,
+	cfloat FLOAT,
+	cdouble DOUBLE PRIMARY KEY
 );
 
