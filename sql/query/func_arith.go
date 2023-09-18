3ba252f1434a5883cf0cad74d8b3d9521ab39da7
Satoshi Konno <skonno@cybergarage.org>
2023-09-18 15:50:23 +0900

    Update query.NewArithFunctionWith()

--- sql/query/func_arith.go
+++ sql/query/func_arith.go
@@ -15,7 +15,6 @@
 package query
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/cybergarage/go-safecast/safecast"
@@ -26,28 +25,21 @@ type ArithFunc func(any, any) (any, error)
 
 // ArithFunction represents a base arithmetic function.
 type ArithFunction struct {
-	column   string
-	args     []any
+	name     string
 	operator ArithFunc
 }
 
 // NewArithFunctionWith returns a new base arithmetic function with the specified name and arithmetic.
-func NewArithFunctionWith(name string, args []any, fn ArithFunc) *ArithFunction {
+func NewArithFunctionWith(name string, fn ArithFunc) *ArithFunction {
 	return &ArithFunction{
-		column:   strings.ToUpper(name),
-		args:     args,
+		name:     strings.ToUpper(name),
 		operator: fn,
 	}
 }
 
 // Name returns the name of the function.
 func (fn *ArithFunction) Name() string {
-	return fn.column
-}
-
-// ColumnName returns the name of the column.
-func (fn *ArithFunction) ColumnName() string {
-	return fn.column
+	return fn.name
 }
 
 // Type returns the type of the function.
@@ -58,28 +50,15 @@ func (fn *ArithFunction) Type() FunctionType {
 // Execute returns the executed value with the specified arguments.
 func (fn *ArithFunction) Execute(args ...any) (any, error) {
 	if len(args) != 2 {
-		return nil, newErrInvalidArguments(fn.column, args)
+		return nil, newErrInvalidArguments(fn.name, args)
 	}
 	return fn.operator(args[0], args[1])
 }
 
-// SelectorString returns the selector string representation.
-func (fn *ArithFunction) SelectorString() string {
-	strs := []string{
-		fn.column,
-		"=",
-	}
-	for _, arg := range fn.args {
-		strs = append(strs, fmt.Sprintf("%v", arg))
-	}
-	return strings.Join(strs, " ")
-}
-
 // NewAbsFunction returns a new abs function.
-func NewAddFunction(column string, args []any) *ArithFunction {
+func NewAddFunction(name string, args []any) *ArithFunction {
 	return NewArithFunctionWith(
-		column,
-		args,
+		name,
 		func(v1, v2 any) (any, error) {
 			fv1, fv2, err := newArithNumericArgsFrom(v1, v2)
 			if err != nil {
