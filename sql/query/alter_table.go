8c77beb379439a8079b234c76edc5be0617465d7
Satoshi Konno <skonno@cybergarage.org>
2023-09-28 16:08:07 +0900

    Update AlterTable::String()

--- sql/query/alter_table.go
+++ sql/query/alter_table.go
@@ -170,6 +170,11 @@ func (stmt *AlterTable) String() string {
 		elems = append(elems, c.DefinitionString())
 
 	}
+	if i, ok := stmt.AddIndex(); ok {
+		elems = append(elems, "ADD")
+		elems = append(elems, i.DefinitionString())
+
+	}
 	if c, ok := stmt.DropColumn(); ok {
 		elems = append(elems,
 			[]string{
