67d12170792b3d5a38655e86a325cd7e731013cc
Satoshi Konno <skonno@cybergarage.org>
2024-11-09 11:55:34 +0900

    Update query.NewDataDef()

--- sql/query/data.go
+++ sql/query/data.go
@@ -25,8 +25,8 @@ type DataDef struct {
 	Length int
 }
 
-// NewDataDefinitionWith returns a new DataType instance with the specified type and length.
-func NewDataDefinitionWith(t DataType, l int) *DataDef {
+// NewDataDef returns a new DataType instance with the specified type and length.
+func NewDataDef(t DataType, l int) *DataDef {
 	return &DataDef{
 		Type:   t,
 		Length: l,
@@ -38,13 +38,13 @@ func NewDataDefinitionFrom(s string, l int) (*DataDef, error) {
 	us := strings.ToUpper(s)
 	for dataType, dataTypeString := range dataTypeStrings {
 		if dataTypeString == us {
-			return NewDataDefinitionWith(dataType, l), nil
+			return NewDataDef(dataType, l), nil
 		}
 	}
 	for dataType, dataTypeString := range dataTypeStrings {
 		dataTypeString = strings.ReplaceAll(dataTypeString, " ", "")
 		if dataTypeString == us {
-			return NewDataDefinitionWith(dataType, l), nil
+			return NewDataDef(dataType, l), nil
 		}
 	}
 	return nil, fmt.Errorf("%w data type : %s", ErrInvalid, s)
