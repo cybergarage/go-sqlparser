4b4cf8054c84524cc9b41b025105b7ab10341832
Satoshi Konno <skonno@cybergarage.org>
2025-01-26 21:49:29 +0900

    Update insert.sql

---
+++ sqltest/res/sql/sqltest/insert.sql
@@ -0,0 +1,37 @@
+CREATE TABLE test_int (
+CREATE TABLE test (
+CREATE TABLE test (
+CREATE TABLE test_int (
+CREATE TABLE test_float (
+CREATE TABLE test (
+CREATE TABLE test_float (
+CREATE TABLE test_float (
+CREATE TABLE test_int (
+CREATE TABLE test (
+CREATE TABLE test (
+CREATE TABLE test_double (
+CREATE TABLE test (
+CREATE INDEX textidx ON test(ctext);
+CREATE TABLE test (
+CREATE TABLE test_int (
+CREATE TABLE test (
+CREATE TABLE usertable (
+CREATE TABLE test (
+CREATE TABLE test_int (
+CREATE TABLE test_double (
+CREATE TABLE test (
+CREATE INDEX intidx ON test(cint);
+CREATE TABLE test (
+CREATE TABLE test (
+CREATE INDEX floatidx ON test(cfloat);
+CREATE TABLE test_double (
+CREATE TABLE test (
+CREATE INDEX doubleidx ON test(cdouble);
+CREATE TABLE test (
+CREATE INDEX datetimeidx ON test(cdatetime);
+CREATE TABLE test_float (
+CREATE TABLE test_double (
+CREATE TABLE test_float (
+CREATE TABLE test (
+CREATE TABLE test_double (
+CREATE TABLE test (
